c6b112319733c24d170a3b96c4ee72bae218028e
tutanon sinthuprasith <tutanon@cern.ch>
2017-03-29 18:37:05 +0200

    Add HLT_PFHT900 for 2016H

--- src/BHAnalyzerTLBSM.cc
+++ src/BHAnalyzerTLBSM.cc
@@ -283,6 +283,8 @@ class BHAnalyzerTLBSM : public edm::EDAnalyzer {
 		//bool firedHLT_PFHT650_v2;
 		bool firedHLT_PFHT475;
 		bool firedHLT_PFHT800;
+		bool firedHLT_PFHT900;
+		bool firedHLT_Mu50;
 		// Addition triggers for 2016H FW problem
 		bool firedHLT_CaloJet500_NoJetID;
 		bool firedHLT_AK8PFJet450       ;
@@ -611,6 +613,8 @@ BHAnalyzerTLBSM::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
 		//firedHLT_PFHT650_v2  = false;
 		firedHLT_PFHT475  = false;
 		firedHLT_PFHT800  = false;
+		firedHLT_PFHT900  = false;
+		firedHLT_Mu50  = false;
 		// Addition triggers for 2016H FW problem
 		firedHLT_CaloJet500_NoJetID  = false;
 		firedHLT_AK8PFJet450         = false;
@@ -670,6 +674,8 @@ BHAnalyzerTLBSM::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
 			//if( triggerList[i] == "HLT_PFHT800_v3")  { firedHLT_PFHT800_v3  = true; }
 			if( triggerList[i].find("HLT_PFHT475")!=std::string::npos)  { firedHLT_PFHT475 = true; }
 			if( triggerList[i].find("HLT_PFHT800")!=std::string::npos)  { firedHLT_PFHT800 = true; }
+			if( triggerList[i].find("HLT_PFHT900")!=std::string::npos)  { firedHLT_PFHT900 = true; }
+			if( triggerList[i].find("HLT_Mu50")!=std::string::npos)  { firedHLT_Mu50 = true; }
 			if( triggerList[i].find("HLT_CaloJet500_NoJetID")!=std::string::npos)  { firedHLT_CaloJet500_NoJetID = true; }
 			if( triggerList[i].find("HLT_AK8PFJet450")!=std::string::npos)  { firedHLT_AK8PFJet450        = true; }
 			if( triggerList[i].find("HLT_PFJet450")!=std::string::npos)  { firedHLT_PFJet450           = true; }
@@ -1206,6 +1212,8 @@ void BHAnalyzerTLBSM::beginJob()
 	//tree->Branch("firedHLT_PFHT600_v2",&firedHLT_PFHT600_v2,"firedHLT_PFHT600_v2/O");
 	tree->Branch("firedHLT_PFHT475" ,  &firedHLT_PFHT475 ,  "firedHLT_PFHT475/O" );
 	tree->Branch("firedHLT_PFHT800" ,  &firedHLT_PFHT800 ,  "firedHLT_PFHT800/O" );
+	tree->Branch("firedHLT_PFHT900" ,  &firedHLT_PFHT900 ,  "firedHLT_PFHT900/O" );
+	tree->Branch("firedHLT_Mu50" ,  &firedHLT_Mu50 ,  "firedHLT_Mu50/O" );
 	tree->Branch("firedHLT_CaloJet500_NoJetID" ,  &firedHLT_CaloJet500_NoJetID ,  "firedHLT_CaloJet500_NoJetID/O" );
 	tree->Branch("firedHLT_AK8PFJet450"        ,  &firedHLT_AK8PFJet450        ,  "firedHLT_AK8PFJet450/O" );
 	tree->Branch("firedHLT_PFJet450"           ,  &firedHLT_PFJet450           ,  "firedHLT_PFJet450/O" );
